d22010d2ba2586dc08d14553d12c92335a40bbbd
Arpik Sargsyan <arpi@xamlab.com>
2018-11-08 13:07:45 +0400

    Added check in action and deselect row

--- FoodPinCustomTable/FoodPin/RestaurantTableViewController.swift
+++ FoodPinCustomTable/FoodPin/RestaurantTableViewController.swift
@@ -18,6 +18,8 @@ class RestaurantTableViewController: UITableViewController {
     
     var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian / Causual Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American / Seafood", "American", "American", "Breakfast & Brunch", "Coffee & Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
     
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -48,14 +50,50 @@ class RestaurantTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-            let optionMenu = UIAlertController(title: nil,
-                                               message: "What do you want to do?",
-                                               preferredStyle: .actionSheet)
-            let cancelAction = UIAlertAction(title: "Cancel",
-                                             style: .cancel,
-                                             handler: nil )
-            optionMenu.addAction(cancelAction)
-        
+        let optionMenu = UIAlertController(title: nil,
+                                           message: "What do you want to do?",
+                                           preferredStyle: .actionSheet)
+        let cancelAction = UIAlertAction(title: "Cancel",
+                                         style: .cancel,
+                                         handler: nil )
+        optionMenu.addAction(cancelAction)
+    
         present(optionMenu, animated: true, completion: nil)
+        
+        let callActionHandler = {
+            (action: UIAlertAction!) -> Void in
+            let alertMessage = UIAlertController(title: "Service Unavailable",
+                                                 message: "Sorry the call feature is not available yet.",
+                                                 preferredStyle: .alert)
+            alertMessage.addAction(UIAlertAction(title: "Ok",
+                                                 style: .default,
+                                                 handler: nil))
+            
+            self.present(alertMessage, animated: true, completion: nil)
+        }
+        
+        let callAction = UIAlertAction(title: "Call" + "123-000-\(indexPath.row)",
+                                       style: .default,
+                                       handler: callActionHandler)
+        
+        optionMenu.addAction(callAction)
+        
+        // Check in action
+        
+        let checkInAction = UIAlertAction(title: "Check in",
+                                          style: .default,
+                                          handler: { (action:UIAlertAction!) -> Void in
+                                            
+                                          let cell = tableView.cellForRow(at: indexPath)
+                                          cell?.accessoryType = .checkmark
+        })
+        
+        // Display the menu
+        
+        optionMenu.addAction(checkInAction)
+        
+        // Deselect the row
+        
+        tableView.deselectRow(at: indexPath, animated: false)
     }
 }
