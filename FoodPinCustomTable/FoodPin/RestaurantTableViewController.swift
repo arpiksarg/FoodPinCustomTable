45cc05c09c65b471b1c6666e2750a1d309124024
Arpik Sargsyan <arpi@xamlab.com>
2018-11-12 14:26:33 +0400

    Copied selected restaurant image when user hit share -> copy

--- FoodPinCustomTable/FoodPin/RestaurantTableViewController.swift
+++ FoodPinCustomTable/FoodPin/RestaurantTableViewController.swift
@@ -58,7 +58,19 @@ class RestaurantTableViewController: UITableViewController {
         let shareAction = UIContextualAction(style: .normal, title: "Share") { (action, sourceView, completionHandler) in
             
             let defaultText = "Just checking in at " + self.restaurantNames[indexPath.row]
-            let activityController = UIActivityViewController(activityItems: [defaultText], applicationActivities: nil)
+            let activityController: UIActivityViewController
+            if let imageToShare = UIImage(named: self.restaurantImages[indexPath.row]) {
+                
+                activityController = UIActivityViewController(activityItems: [defaultText, imageToShare], applicationActivities: nil)
+            
+            } else {
+             
+                activityController = UIActivityViewController(activityItems: [defaultText], applicationActivities: nil)
+           
+            }
+            
+            
+            //= UIActivityViewController(activityItems: [defaultText], applicationActivities: nil)
             
             self.present(activityController, animated: true, completion: nil)
             
