473a93ff60945e2e17ce69fa79611176d75bca30
Arpik Sargsyan <arpi@xamlab.com>
2018-11-22 15:16:59 +0400

    Added map to a cell#4

---
+++ FoodPinCustomTable/FoodPin/View/RestaurantDetailSeparatorCell.swift
@@ -0,0 +1,26 @@
+//
+//  RestaurantDetailSeparatorCell.swift
+//  FoodPin
+//
+//  Created by Arpik Sargsyan on 11/22/18.
+//  Copyright © 2018 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantDetailSeparatorCell: UITableViewCell {
+
+    @IBOutlet var titleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
