25a99e736f646570990374efb053aed364c5c79e
Arpik Sargsyan <arpi@xamlab.com>
2018-11-26 19:04:49 +0400

    Passed restaurant object from the Detail view to the Review view by checking segue identifier

--- FoodPinCustomTable/FoodPin/Controller/RestaurantDetailViewController.swift
+++ FoodPinCustomTable/FoodPin/Controller/RestaurantDetailViewController.swift
@@ -113,6 +113,10 @@ class RestaurantDetailViewController: UIViewController, UITableViewDataSource, U
         if segue.identifier == "showMap" {
             let destinationController = segue.destination as! MapViewController
             destinationController.restaurant = restaurant
+        
+        } else if segue.identifier == "showReview" {
+            let destinationController = segue.destination as! ReviewViewController
+            destinationController.restaurant = restaurant
         }
     }
 }
