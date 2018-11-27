d295791ff8eac3bfc056470f128977b55adf5096
Arpik Sargsyan <arpi@xamlab.com>
2018-11-27 17:03:44 +0400

    Added spring animation to the rateButtons

--- FoodPinCustomTable/FoodPin/Controller/ReviewViewController.swift
+++ FoodPinCustomTable/FoodPin/Controller/ReviewViewController.swift
@@ -39,22 +39,25 @@ class ReviewViewController: UIViewController {
 
 
 override func viewWillAppear(_ animated: Bool) {
-//        UIView.animate(withDuration: 2.0) {
-//            self.rateButtons[0].alpha = 1.0
-//            self.rateButtons[1].alpha = 1.0
-//            self.rateButtons[2].alpha = 1.0
-//            self.rateButtons[3].alpha = 1.0
-//            self.rateButtons[4].alpha = 1.0
-        
-            UIView.animate(withDuration: 0.4, delay: 0.1, options: [], animations: {self.rateButtons[0].alpha = 1.0
-                                                                                    self.rateButtons[0].transform = .identity }, completion: nil)
-            UIView.animate(withDuration: 0.4, delay: 0.2, options: [], animations: {self.rateButtons[1].alpha = 1.0
-                                                                                     self.rateButtons[1].transform = .identity}, completion: nil)
-            UIView.animate(withDuration: 0.4, delay: 0.3, options: [], animations: {self.rateButtons[2].alpha = 1.0
-                                                                                    self.rateButtons[2].transform = .identity}, completion: nil)
-            UIView.animate(withDuration: 0.4, delay: 0.4, options: [], animations: {self.rateButtons[3].alpha = 1.0}, completion: nil)
-                                                                                     self.rateButtons[3].transform = .identity
-            UIView.animate(withDuration: 0.4, delay: 0.5, options: [], animations: {self.rateButtons[4].alpha = 1.0
-                                                                                    self.rateButtons[4].transform = .identity}, completion: nil)
+    
+        UIView.animate(withDuration: 0.8, delay: 0.1, usingSpringWithDamping: 0.2, initialSpringVelocity: 0.3, options: [], animations: {
+            self.rateButtons[0].alpha = 1.0
+            self.rateButtons[0].transform = .identity }, completion: nil)
+    
+        UIView.animate(withDuration: 0.8, delay: 0.1, usingSpringWithDamping: 0.2, initialSpringVelocity: 0.3, options: [], animations: {
+            self.rateButtons[1].alpha = 1.0
+            self.rateButtons[1].transform = .identity }, completion: nil)
+    
+        UIView.animate(withDuration: 0.8, delay: 0.1, usingSpringWithDamping: 0.2, initialSpringVelocity: 0.3, options: [], animations: {
+            self.rateButtons[2].alpha = 1.0
+            self.rateButtons[2].transform = .identity }, completion: nil)
+    
+        UIView.animate(withDuration: 0.8, delay: 0.1, usingSpringWithDamping: 0.2, initialSpringVelocity: 0.3, options: [], animations: {
+            self.rateButtons[3].alpha = 1.0
+            self.rateButtons[3].transform = .identity }, completion: nil)
+    
+        UIView.animate(withDuration: 0.8, delay: 0.1, usingSpringWithDamping: 0.2, initialSpringVelocity: 0.3, options: [], animations: {
+            self.rateButtons[4].alpha = 1.0
+            self.rateButtons[4].transform = .identity }, completion: nil)
     }
 }
