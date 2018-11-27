0d6f2f2368e10c0463289c08e7d431385a01b3c0
Arpik Sargsyan <arpi@xamlab.com>
2018-11-27 15:44:53 +0400

    Added fade in animation to the rating buttons

--- FoodPinCustomTable/FoodPin/Controller/ReviewViewController.swift
+++ FoodPinCustomTable/FoodPin/Controller/ReviewViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ReviewViewController: UIViewController {
 
     @IBOutlet var backgroundImageView: UIImageView!
-    @IBOutlet var rateButton: [UIButton]
+    @IBOutlet var rateButtons: [UIButton]!
     
     var restaurant = Restaurant()
     
@@ -26,5 +26,19 @@ class ReviewViewController: UIViewController {
         let blurEffectView = UIVisualEffectView(effect: blurEffect)
         blurEffectView.frame = view.bounds
         backgroundImageView.addSubview(blurEffectView)
+        
+        for rateButton in rateButtons {
+            rateButton.alpha = 0
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        UIView.animate(withDuration: 2.0) {
+            self.rateButtons[0].alpha = 1.0
+            self.rateButtons[1].alpha = 1.0
+            self.rateButtons[2].alpha = 1.0
+            self.rateButtons[3].alpha = 1.0
+            self.rateButtons[4].alpha = 1.0
+        }
     }
 }
