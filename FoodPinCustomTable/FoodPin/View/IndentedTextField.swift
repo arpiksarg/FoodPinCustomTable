c8f65529a348712c7786cb0e04754cdf1f7a2927
Arpik Sargsyan <arpi@xamlab.com>
2018-11-30 13:11:10 +0400

    Added new file to View for Placeholder text indentation

---
+++ FoodPinCustomTable/FoodPin/View/IndentedTextField.swift
@@ -0,0 +1,14 @@
+//
+//  IndentedTextField.swift
+//  FoodPin
+//
+//  Created by Arpik Sargsyan on 11/30/18.
+//  Copyright © 2018 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class IndentedTextField: UITextField {
+    
+
+}
