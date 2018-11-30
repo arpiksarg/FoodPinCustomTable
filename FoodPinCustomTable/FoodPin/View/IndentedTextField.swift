df14e1e258b4cd88040d45b786d3c5c60a048dea
Arpik Sargsyan <arpi@xamlab.com>
2018-11-30 13:26:34 +0400

    Added Text Fields methods and override them to return rectangle with indentation

--- FoodPinCustomTable/FoodPin/View/IndentedTextField.swift
+++ FoodPinCustomTable/FoodPin/View/IndentedTextField.swift
@@ -10,5 +10,23 @@ import UIKit
 
 class IndentedTextField: UITextField {
     
-
+    let padding = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+    
+    // returns the drawing rectangle for the text field's text
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+            return bounds.inset(by: padding)
+        }
+    
+    // returns the drawing rectangle for the text field's placeholder text
+    
+    override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+    
+    // returns the rectangle in which editable text can be displayed
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
 }
