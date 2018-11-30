2badf67d1c4ba30a5ac3b38000f9436e668c79be
Arpik Sargsyan <arpi@xamlab.com>
2018-11-30 14:34:19 +0400

    Added new file NewRestaurantController to work with text field

---
+++ FoodPinCustomTable/FoodPin/Controller/NewRestaurantController.swift
@@ -0,0 +1,18 @@
+//
+//  NewRestaurantController.swift
+//  FoodPin
+//
+//  Created by Arpik Sargsyan on 11/30/18.
+//  Copyright © 2018 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class NewRestaurantController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
