84b3eb008fdafc2c39d3382ccc614c9e97a49404
Arpik Sargsyan <arpi@xamlab.com>
2018-12-14 15:00:22 +0400

    Adopted UISearchResultsUpdating protocol to update the search results

--- FoodPinCustomTable/FoodPin/Controller/RestaurantTableViewController.swift
+++ FoodPinCustomTable/FoodPin/Controller/RestaurantTableViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import CoreData
 
-class RestaurantTableViewController: UITableViewController, NSFetchedResultsControllerDelegate {
+class RestaurantTableViewController: UITableViewController, NSFetchedResultsControllerDelegate, UISearchResultsUpdating {
     
     var restaurants:[RestaurantMO] = []
     
