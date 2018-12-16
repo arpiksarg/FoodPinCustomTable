302c063b987690becf3353b67a57f925c86b3da4
Arpik Sargsyan <arpi@xamlab.com>
2018-12-16 22:42:54 +0400

    Added swift file for Walkthrough controller and IBOutlets

---
+++ FoodPinCustomTable/FoodPin/Controller/WalkthroughContentViewController.swift
@@ -0,0 +1,37 @@
+//
+//  WalkthroughContentViewController.swift
+//  FoodPin
+//
+//  Created by Arpik Sargsyan on 12/16/18.
+//  Copyright © 2018 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class WalkthroughContentViewController: UIViewController {
+    
+    @IBOutlet var headingLabel: UILabel! {
+        didSet {
+            headingLabel.numberOfLines = 0
+        }
+    }
+    
+    @IBOutlet var subheadingLabel: UILabel! {
+        didSet {
+            subheadingLabel.numberOfLines = 0
+        }
+    }
+    
+    @IBOutlet var contentImageVeiw: UIImageView!
+    
+    var index = 0
+    var heading = ""
+    var subHeading = ""
+    var imageFile = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
