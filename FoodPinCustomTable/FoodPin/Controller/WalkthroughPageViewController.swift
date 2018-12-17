e98bfac423b67c26c3bb2a463694dd18c31ffe3b
Arpik Sargsyan <arpi@xamlab.com>
2018-12-17 13:41:44 +0400

    Added method to create the page view controller on demand

--- FoodPinCustomTable/FoodPin/Controller/WalkthroughPageViewController.swift
+++ FoodPinCustomTable/FoodPin/Controller/WalkthroughPageViewController.swift
@@ -10,7 +10,21 @@ import UIKit
 
 class WalkthroughPageViewController: UIPageViewController, UIPageViewControllerDataSource {
    
+    var pageHeadings = ["CREATE YOUR OWN FOOD GUIDE", "SHOW YOU THE LOCATION", "DISCOVER GREAT RESTAURANTS"]
+    var pageImages = ["onboarding-1", "onboarding-2", "onboarding-3"]
+    var pageSubheadings = ["Pin your favorite restaurants and craet your own food guide",
+                           "Search and locate your favorite restaurant on Maps",
+                           "Find restaurants shared by your friends and other foodies"]
+    var currentIndex = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        // get the current page index of the given view controller
+        
         var index = (viewController as! WalkthroughContentViewController).index
         index -= 1
         
@@ -18,22 +32,36 @@ class WalkthroughPageViewController: UIPageViewController, UIPageViewControllerD
     }
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        
+        // get the current page index of the given view controller
+        
         var index = (viewController as! WalkthroughContentViewController).index
         index += 1
         
         return contentViewController(at: index)
     }
     
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    // helper method that is designed to create the page view controller on demand
+    
+    func contentViewController(at index: Int) -> WalkthroughContentViewController? {
         
-        var pageHeadings = ["CREATE YOUR OWN FOOD GUIDE", "SHOW YOU THE LOCATION", "DISCOVER GREAT RESTAURANTS"]
-        var pageImages = ["onboarding-1", "onboarding-2", "onboarding-3"]
-        var pageSubheadings = ["Pin your favorite restaurants and craet your own food guide",
-                               "Search and locate your favorite restaurant on Maps",
-                               "Find restaurants shared by your friends and other foodies"]
-        var currentIndex = 0
-
+        if index < 0 || index >= pageHeadings.count {
+            return nil
+        }
+        
+        // Create a new view controller and pass suitable datal
+        
+        let storyboard = UIStoryboard(name: "Oboarding", bundle: nil)
+        if let pageContentViewController = storyboard.instantiateViewController(withIdentifier: "WalkthroughContentViewController") as? WalkthroughContentViewController {
+            
+            pageContentViewController.imageFile = pageImages[index]
+            pageContentViewController.heading = pageHeadings[index]
+            pageContentViewController.subHeading = pageSubheadings[index]
+            pageContentViewController.index = index
+            
+            return pageContentViewController
+        }
+        
+        return nil
     }
 }
