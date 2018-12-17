573c64ccfbb2141d5a8d60e4acc88d7919f4ab46
Arpik Sargsyan <arpi@xamlab.com>
2018-12-17 13:12:54 +0400

    Added swift file for the page view controller

---
+++ FoodPinCustomTable/FoodPin/Controller/WalkthroughPageViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WalkthroughPageViewController.swift
+//  FoodPin
+//
+//  Created by Arpik Sargsyan on 12/17/18.
+//  Copyright © 2018 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class WalkthroughPageViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
