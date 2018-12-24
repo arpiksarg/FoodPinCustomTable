bc30ab303dd3b46514fabb2591d2b4d06ce82b01
Arpik Sargsyan <arpi@xamlab.com>
2018-12-24 19:17:05 +0400

    Declared Outlet variable and the property for storing the target URL

--- FoodPinCustomTable/FoodPin/Controller/WebViewController.swift
+++ FoodPinCustomTable/FoodPin/Controller/WebViewController.swift
@@ -7,9 +7,14 @@
 //
 
 import UIKit
+import WebKit
 
 class WebViewController: UIViewController {
-
+    
+    @IBOutlet weak var webView: WKWebView!
+    var targetURL = ""
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
